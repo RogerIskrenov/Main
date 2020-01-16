3b9ef40dc8fe8711cff39502df620492df7d3625
Roger <rogersamson676@gmail.com>
2020-01-16 21:57:41 +0200

    Task 3
    This is a small game that let's you guess what random number did the computer chose.
    If you get the number right you win. If you don't get it right the first time you have 5 more tries to go.
    If you guess wrong all 6 of your tries you lose the game. 
    Also between every try you can hose to surrender or keep playing.

---
+++ Task 3.cpp
@@ -0,0 +1,42 @@
+﻿/* 1. Компютъра си измисля случайно число от 1 до 100.
+   2. Ти се опитваш да го познаеш.
+   3. След всеки опит ти казва дали си познал, или числото е по - голямо или по - малко от това, което си предположил.
+   4. Брои опитите и ако направиш повече от 6, губиш.
+   5. А, и при всяко отгатване да имаш начин да се откажеш и да губиш играта веднага.*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+	int t = 0; //number of times the player tried to win
+	int r; //the random number
+	int v; //the players number
+	int o = 1; //choise to surrender
+
+	while (t < 6, o==1) {
+		t++;
+		r = rand() % 100 + 1; //gets random number from 1 to 100 
+		cout << "Enter a number \n";
+		cin >> v; //enters a number
+		if (r > v) {
+			cout << "Your number is smaller, try again. \n";
+		}
+		if (r < v) {
+			cout << "Your number is higher, try again. \n";
+		}
+		if (r == v) {
+			cout << "Numbers are equal. You win. \n";
+			return 0;
+		}
+		if (o == 1) {
+			cout << "Do you want to surrender? Press 0 for 'yes' and 1 for 'no' \n";
+			cin >> o;
+		}
+		if (t == 6) {
+			cout << "You lost";
+			return 0;
+		}
+	}		
+}
+	
+	
